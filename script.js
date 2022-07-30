9f6e8582b8519cd553825dd3d27c661c0fcf61e8
Srajan <100074395+SrajanDongre@users.noreply.github.com>
2022-07-30 10:42:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+var countdown=document.getElementById("countdown");
+countdown.setAttribute("class","countdown")
+countdown.setAttribute("style","fontsize:25px")
+setTimeout(()=>{
+    countdown.innerHTML="10"
+    setTimeout(()=>{
+        countdown.innerHTML=""
+        countdown.innerHTML="9"
+        setTimeout(()=>{
+            countdown.innerHTML=""
+            countdown.innerHTML="8"
+            setTimeout(()=>{
+                countdown.innerHTML=""
+                countdown.innerHTML="7"
+                setTimeout(()=>{
+                    countdown.innerHTML=""
+                    countdown.innerHTML="6"
+                    setTimeout(()=>{
+                        countdown.innerHTML=""
+                        countdown.innerHTML="5"
+                        setTimeout(()=>{
+                            countdown.innerHTML=""
+                            countdown.innerHTML="4"
+                            setTimeout(()=>{
+                                countdown.innerHTML=""
+                                countdown.innerHTML="3"
+                                setTimeout(()=>{
+                                    countdown.innerHTML=""
+                                    countdown.innerHTML="2"
+                                    setTimeout(()=>{
+                                        countdown.innerHTML=""
+                                        countdown.innerHTML="1"
+                                        setTimeout(()=>{
+                                            countdown.innerHTML=""
+                                            countdown.innerHTML="Happy Independence Day" 
+                                        }, 1000);                                           
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
